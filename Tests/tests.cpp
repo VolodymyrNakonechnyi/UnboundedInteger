94c1daaaec0335d057176c3178a203622f3dce46
VolodymyrNakonechnyi <146861887+VolodymyrNakonechnyi@users.noreply.github.com>
2023-10-19 15:16:56 +0300

    Update tests.cpp

--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -1,4 +1,9 @@
 #include "../UnboundedInteger/UnboundedInteger.h"
+#include <iostream>
+#include <cstdint>
+#include <string>
+#include <sstream>
+#include <iomanip>
 
 void testXOR() {
     UnboundedInteger numberA("51bf608414ad5726a3c1bec098f77b1b54ffb2787f8d528a74c1d7fde6470ea4");
